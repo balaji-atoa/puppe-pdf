48ac8fecbe5d82fc3a52f26e4a8080f8c0bf9bf1
balaji-atoa <balaji@paywithatoa.co.uk>
2024-09-29 14:13:38 +0530

    add tests

--- test/core.js
+++ test/core.js
@@ -1,7 +1,10 @@
 const test = require('tape')
+const fs = require('node:fs')
+const path = require('node:path')
+const pdfParse = require('pdf-parse')
 const puppePdf = require('../src/index')
 const { TEST_URL_TO_EXPORT } = require('../src/constants')
-const pdfParse = require('pdf-parse')
+const baseHtml = fs.readFileSync(path.join(__dirname, 'html', 'base.html'), 'utf8')
 
 test('directly passing string to forgePdf should convert the website2pdf', async (t) => {
   t.plan(2)
@@ -23,4 +26,28 @@ test('provide only url', async (t) => {
 
   t.equal(parsed.info.Title, 'What is Puppeteer? | Puppeteer')
   t.equal(parsed.info.Creator, 'Chromium')
+})
+
+test('provide a html', async (t) => {
+  t.plan(2)
+
+  const pdf = await puppePdf.forgePDF({
+    html: baseHtml
+  })
+
+  const parsed = await pdfParse(pdf)
+
+  t.equal(parsed.info.Title, 'Puppe Pdf > Test Base functionality')
+  t.ok(parsed.text.includes('welcome to PuppePdf'))
+})
+
+test('dont provide url or html should throw error', async (t) => {
+  t.plan(1)
+
+  try {
+    await puppePdf.forgePDF()
+    t.fail('did not work as expected')
+  } catch (error) {
+    error.message.match(/Options should be an object or string/) ? t.pass('throws as expected') : t.fail('did not work as expected. Check error message')
+  }
 })
