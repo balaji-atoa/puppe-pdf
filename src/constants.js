4daefbd164a6b603ed98072bcdc179399eae2bae
balaji-atoa <balaji@paywithatoa.co.uk>
2024-09-30 13:17:14 +0530

    puppe args optimize

--- src/constants.js
+++ src/constants.js
@@ -4,8 +4,27 @@ const DEFAULT_PDF_OPTIONS = {
 }
 const DEFAULT_LAUNCH_OPTIONS = {
   headless: 'shell', // using shell mode as it is the fastest
-  args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-extensions']
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-extensions',
+    '--no-first-run',
+    '--no-sandbox',
+    '--no-zygote',
+    '--disable-notifications',
+    '--disable-geolocation',
+    '--single-process',
+    '--disable-dev-shm-usage',
+    '--disable-background-networking',
+    '--disable-background-timer-throttling',
+    '--disable-default-apps',
+    '--disable-crash-reporter',
+    '--disable-software-rasterizer',
+    '--metrics-recording-only',
+    '--enable-fast-unload',
+    '--disable-print-preview',
+    '--disable-breakpad'
+  ]
 }
-const DEFAULT_JAVASCRIPT_SETTING = true
 const TEST_URL_TO_EXPORT = 'https://pptr.dev/guides/what-is-puppeteer'
-module.exports = { DEFAULT_PDF_OPTIONS, DEFAULT_LAUNCH_OPTIONS, DEFAULT_JAVASCRIPT_SETTING, TEST_URL_TO_EXPORT }
+module.exports = { DEFAULT_PDF_OPTIONS, DEFAULT_LAUNCH_OPTIONS, TEST_URL_TO_EXPORT }
